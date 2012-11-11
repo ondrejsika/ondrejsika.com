091a3742087934acdb0d3cc8050cfec07658ba7b
Ondrej Sika <sika.ondrej@gmail.com>
2012-11-11 17:47:08 +0100

    init base pages

--- app.py
+++ app.py
@@ -10,9 +10,24 @@ execfile(activate_this, dict(__file__=activate_this))
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+
+##########
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+##########
 
 if __name__ == '__main__':
     app.run()
