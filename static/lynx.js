f5ed007f5db10e037b96f5abc4bcf3409981ba01
Ondrej Sika <ondrej@ondrejsika.com>
2013-10-17 23:37:08 +0200

    add #lynx view

--- static/lynx.js
+++ static/lynx.js
@@ -8,3 +8,12 @@ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+if (window.location.hash == "#lynx") {
+    document.getElementsByTagName("h1")[0].outerHTML = "<center>"+document.getElementsByTagName("h1")[0].outerHTML+"</center>"
+    css = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("href", "/static/lynx-lynx.css");
+    document.getElementsByTagName("head")[document.getElementsByTagName("head").length-1].appendChild(css);
+}
