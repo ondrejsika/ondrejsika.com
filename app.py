dd4a4246d1723790f4ddcac858c5ef8444c0414a
Ondrej Sika <ondrej@ondrejsika.com>
2013-11-03 16:12:06 +0100

    rm old part of site, host by GitHub

--- app.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-from wsgi import application
-if __name__ == "__main__":
-    application.debug = True
-    application.run()
--- wsgi.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-from flask_server import FlaskServer, normpath
-application = FlaskServer(__name__, normpath(__file__, ".."))
