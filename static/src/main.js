bff1a265ef59456c10d7e68416b3da6b4da45506
Ondrej Sika <ondrej@ondrejsika.com>
2013-12-25 11:35:22 +0100

    replace `blank` to `_blank`

--- static/src/main.js
+++ static/src/main.js
@@ -28,5 +28,5 @@ var disqus_shortname = 'ondrejsika';
 links = document.getElementsByTagName("a")
 for(i = 0; i< links.length; i++){
     if (links[i].class && links[i].class.search(/no-taget/) == -1) continue
-    if (links[i].hostname != "localhost" && links[i].hostname != "ondrejsika.com") links[i].target = "blank"
+    if (links[i].hostname != "localhost" && links[i].hostname != "ondrejsika.com") links[i].target = "_blank"
 }
