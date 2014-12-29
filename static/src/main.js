796730fd405e877848f81245f608079827ff91ac
Ondrej Sika <ondrej@ondrejsika.com>
2014-12-29 15:23:21 +0100

    add cdn domain to ignore for blank links

--- static/src/main.js
+++ static/src/main.js
@@ -16,6 +16,6 @@ var disqus_shortname = 'ondrejsika';
 links = document.getElementsByTagName("a")
 for(i = 0; i< links.length; i++){
     if (links[i].class && links[i].class.search(/no-taget/) == -1) continue
-    if (links[i].hostname != "localhost" && links[i].hostname != "ondrejsika.com") links[i].target = "_blank"
+    if (links[i].hostname != "localhost" && links[i].hostname != "ondrejsika.com" && links[i].hostname != "ondrejsika-com.r.worldssl.net") links[i].target = "_blank"
 }
 
