abb659ed110b07cf2921fe647e55e9a3df1dbdce
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-24 09:46:59 +0100

    deploy.sh - Update deploy script for Kubernetes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-git remote | xargs -l git push --all
+IMAGE=ondrejsika/ondrejsika.com:deploy-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(date +%s)
 
+docker build -t $IMAGE .
+docker push $IMAGE
+
+helm upgrade --install ondrejsika-com ondrejsika/one-image --set host=ondrejsika.com --set image=$IMAGE --set changeCause=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
