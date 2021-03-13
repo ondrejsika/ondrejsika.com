763c1793d7494d05d94174b5137b1267bd25e54f
Ondrej Sika <ondrej@ondrejsika.com>
2021-03-13 20:25:36 +0100

    deploy.sh -  Build & push also to ondrejsika/ondrejsika.com:latest

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-IMAGE=ondrejsika/ondrejsika.com:deploy-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(date +%s)
+IMAGE_BASE=ondrejsika/ondrejsika.com
+IMAGE_DEPLOYMENT=$IMAGE_BASE:deploy-$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(date +%s)
 
-docker build -t $IMAGE .
-docker push $IMAGE
+docker build -t $IMAGE_BASE .
+docker tag $IMAGE_BASE $IMAGE_DEPLOYMENT
+docker push $IMAGE_BASE
+docker push $IMAGE_DEPLOYMENT
 
-helm upgrade --install ondrejsika-com ondrejsika/one-image --set host=ondrejsika.com --set image=$IMAGE --set changeCause=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+helm upgrade --install ondrejsika-com ondrejsika/one-image --set host=ondrejsika.com --set image=$IMAGE_DEPLOYMENT --set changeCause=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
