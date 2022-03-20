8c5370cbc004e91ca8901a661b35a5e5153c5f75
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-20 20:32:04 +0100

    chore: Set platform to linux/amd64 in docker build & push script

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -3,6 +3,6 @@
 IMAGE_BASE=ondrejsika/ondrejsika.com
 IMAGE_DEPLOYMENT=$IMAGE_BASE:$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)-$(date +%s)
 
-docker build -t $IMAGE_BASE -t $IMAGE_DEPLOYMENT .
+docker build --platform linux/amd64 -t $IMAGE_BASE -t $IMAGE_DEPLOYMENT .
 docker push $IMAGE_BASE
 docker push $IMAGE_DEPLOYMENT
